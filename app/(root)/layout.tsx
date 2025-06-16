932ba5afad3a22dd3a0668bb2225e60775168815
deepali <deepaliyadav72568@gmail.com>
2025-06-16 16:24:52 +0530

    fix: updated folder structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 // import NavbarComponents from '@/components/navbarComponents';
-import './globals.css';
+import '.././globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
